4dfa937c2530de9fbbf8ef64c184abf4c1744e9c
Jan Jongboom <janjongboom@gmail.com>
2013-09-04 13:12:21 -0400

    Wip

--- plugins/express/express.js
+++ plugins/express/express.js
@@ -1,19 +1,36 @@
 var express = require("express");
 var assert = require("assert");
 
-module.exports = function (options, imports, register) {
-    assert(options.port, "Option 'port' is required");
-    assert(options.host, "Option 'host' is required");
-    
-    var app = express();
-    app.listen(options.port, options.host);
-    
-    app.get("/", function (req, res) {
-        res.writeHead(200);
-        res.end("Hello from Scrapey");
-    });
-    
-    register(null, {
-        express: app
-    });
+module.exports = function(options, imports, register) {
+  assert(options.port, "Option 'port' is required");
+  assert(options.host, "Option 'host' is required");
+
+  var app = express();
+
+  // https://gist.github.com/jonsullivan/3126319
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization, X-Mindflash-SessionID');
+
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+  };
+
+  app.use(allowCrossDomain);
+
+  app.listen(options.port, options.host);
+
+  app.get("/", function(req, res) {
+    res.writeHead(200);
+    res.end("Hello from Scrapey");
+  });
+
+  register(null, {
+    express: app
+  });
 };
--- plugins/parser-http/web.js
+++ plugins/parser-http/web.js
@@ -1,52 +1,126 @@
-var request = require("request");
+var request = require('request');
+var httpProxy = require('http-proxy');
+var Url = require('url');
 
 module.exports = function(app, parser, prefix) {
-    app.all(prefix + "/", function (req, res, next) {
-        if (!req.query.url) return next("?url should be specified");
-        
-        request.get({
-            uri: req.query.url,
-            method: 'GET',
-            encoding: 'utf8'
-        }, function (err, resp, body) {
-            if (err) return next(err);
-            if (resp.statusCode < 200 || resp.statusCode > 300) {
-                return next("Response should be 2xx but was " + resp.statusCode);
-            }
-            
-            parser.parsePage(body, req.query.url, function (err, feed, handlerName) {
-                if (err) return next(err);
-                
-                res.writeHead(200, {
-                    "Content-Type": "application/json; charset=utf8",
-                    "Access-Control-Allow-Origin": "*",
-                    "Scrapey-Handler": handlerName
-                });
-                if (req.query.callback) {
-                    res.end(req.query.callback + "(" + JSON.stringify(feed, null, 4) + ")");
-                }
-                else {
-                    res.end(JSON.stringify(feed, null, 4));
-                }
-            });
-        });
-    });
-    
-    console.log('Listening on', prefix);
-    
-    return {
-        /**
-         * Unregisters the URL
-         */
-        unregister: function() {
-            Object.keys(app.routes).forEach(function(verb) {
-                var route = app.routes[verb].filter(function(r) {
-                    return r.path === prefix + "/";
-                })[0];
-                if (route) {
-                    app.routes[verb].splice(app.routes[verb].indexOf(route), 1);
-                }
-            });
-        }
+  var proxy = new httpProxy.RoutingProxy();
+
+  app.all(prefix + '/', function(req, res, next) {
+    var _next = next;
+    next = function() {
+      res.writeHead = _writeHead;
+      res.write = _write;
+      res.end = _end;
+
+      _next.apply(this, arguments);
     };
+
+    if (!req.query.url) {
+      return next('?url should be specified');
+    }
+
+    var parsedUrl = Url.parse(req.query.url);
+    var proxyOptions = {
+      host: parsedUrl.host,
+      port: parsedUrl.port || 80
+    };
+
+    req.url = parsedUrl.path;
+
+    res.writeHead = function(code) {
+      responseCode = code;
+    };
+
+    var _writeHead = res.writeHead.bind(res);
+    var _write = res.write.bind(res);
+    var _end = res.end.bind(res);
+
+    var buffers = [];
+    var totalLength = 0;
+    var responseCode;
+
+    console.log('proxying', req.url, proxyOptions);
+
+    res.write = function(chunk, encoding) {
+      totalLength += chunk.length;
+      if (totalLength > 2 * 1024 * 1024) {
+        console.log('request too big');
+
+        res.writeHead = _writeHead;
+        res.write = _write;
+        res.end = _end;
+
+        buffers = null;
+
+        return next('Request exceeds maximum of 10 bytes');
+      }
+      console.log('write', chunk.length, encoding);
+      buffers.push(chunk);
+    };
+
+    res.end = function(chunk, encoding) {
+      console.log('end', chunk && chunk.length, encoding);
+      var body = Buffer.concat(buffers).toString('utf8');
+      console.log('i has body', body.substr(0, 100));
+
+      parser.parsePage(body, req.query.url, function(err, feed, handlerName) {
+        if (err) return next(err);
+
+        res.setHeader('Content-Type', 'application/json; charset=utf8');
+        res.setHeader('Scrapey-Handler', handlerName);
+
+        console.log('writing head', responseCode, res._headers);
+        _writeHead(responseCode);
+
+        if (req.query.callback) {
+          _write(req.query.callback + "(" + JSON.stringify(feed, null, 4) + ")");
+        }
+        else {
+          _write(JSON.stringify(feed, null, 4));
+        }
+        _end();
+      });
+    };
+
+    return proxy.proxyRequest(req, res, proxyOptions);
+  });
+
+    // request.get({
+    //   uri: req.query.url,
+    //   method: 'GET',
+    //   encoding: 'utf8'
+    // }, function(err, resp, body) {
+    //   if (err) return next(err);
+    //   if (resp.statusCode < 200 || resp.statusCode > 300) {
+    //     return next("Response should be 2xx but was " + resp.statusCode);
+    //   }
+
+    //   parser.parsePage(body, req.query.url, function(err, feed, handlerName) {
+    //     if (err) return next(err);
+
+    //     res.writeHead(200, {
+    //       "Content-Type": "application/json; charset=utf8",
+    //       "Scrapey-Handler": handlerName
+    //     });
+
+    //   });
+    // });
+
+  console.log('Listening on', prefix);
+
+  return {
+    /**
+     * Unregisters the URL
+     */
+    unregister: function() {
+      Object.keys(app.routes).forEach(function(verb) {
+        var route = app.routes[verb].filter(function(r) {
+          return r.path === prefix + "/";
+        })[0];
+        if (route) {
+          app.routes[verb].splice(app.routes[verb].indexOf(route), 1);
+        }
+      });
+    }
+  };
 };
