d248258a31329348a92332023cfb5fe59dd5e6b3
Jan Jongboom <janjongboom@gmail.com>
2014-10-25 14:48:46 +0100

    Add heritage

---
+++ buckets/heritage/detail.js
@@ -0,0 +1,29 @@
+// http://www.visitukheritage.gov.uk/servlet/com.eds.ir.cto.servlet.CtoDbQueryServlet?location=All&class1=All&freetext=&Submit=search
+
+name = "uk-national-heritage-detail"
+
+matches = function($, location) {
+  return location.indexOf('CtoDetailServlet') !== -1;
+}
+
+extract = {
+  detail: function($) {
+    var table = $('table:nth(2)');
+    var tds = table.find('tr').map(function(ix, tr) {
+      return $(tr).find('td:nth(2)');
+    });
+
+    return {
+      uniqueId: tds[0].text(),
+      category: tds[1].text(),
+      accessDetails: tds[2].text(),
+      contactName: tds[3].text(),
+      contactAddress: tds[4].text(),
+      contactReference: tds[5].text(),
+      telephone: tds[6].text(),
+      fax: tds[7].text(),
+      email: tds[8].text(),
+      description: tds[9].text()
+    };
+  }
+};
---
+++ buckets/heritage/result.js
@@ -0,0 +1,21 @@
+// http://www.visitukheritage.gov.uk/servlet/com.eds.ir.cto.servlet.CtoDbQueryServlet?location=All&class1=All&freetext=&Submit=search
+
+name = "uk-national-heritage"
+
+matches = function($, location) {
+  return location.indexOf('CtoDbQueryServlet') !== -1;
+}
+
+extract = {
+  records: function($, url) {
+    return $('tr[valign=top]:gt(0)').map(function(ix, el) {
+      el = $(el);
+      return {
+        desc: el.find('td:nth(1)').text(),
+        location: el.find('td:nth(2)').text(),
+        category: el.find('td:nth(3)').text(),
+        link: el.find('td:nth(1) a').attr('href')
+      };
+    });
+  }
+};
